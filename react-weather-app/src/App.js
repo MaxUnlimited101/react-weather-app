056d46669a1cbda1bc478de4b8254d3584839857
Maksym Nosal <127298150+nosalm@users.noreply.github.com>
2024-05-01 11:30:48 +0200

    Add basic stuff

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const api = {
+  key: 'a36d358d8dcb236575f6210b2bae325c',
+  base: 'https://api.openweathermap.org/data/3.0/'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+        <div className="location-box">
+          <div className="location">
+
+          </div>
+          <div className="date">
+            
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
